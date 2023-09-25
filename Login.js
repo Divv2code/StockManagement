7e8c3c5bdef4cd287e2fd1a01d24efb835b12efb
Divv2code <145989064+Divv2code@users.noreply.github.com>
2023-09-25 17:28:10 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { auth } from '../firebaseConfig';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      history.push('/admin');
+    } catch (error) {
+      console.error('Error logging in: ', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
